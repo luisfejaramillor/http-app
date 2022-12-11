b276d974d8091b35e2d99ebb4c375c718a2100f6
luisfejaramillor <luisfejaramillor@gmail.com>
2022-12-11 15:29:54 -0500

    first commit

---
+++ src/breakingbad/breaking-bad-app.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ * @param {HTMLDivElement} element
+ */
+
+
+export const BreakingBadApp = (element)=> {
+    document.querySelector('#app-title').innerHTML='Breaking bad App'
+    element.innerHTML='...Loading'
+}
